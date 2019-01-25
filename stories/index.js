77777814a8078238c6fe3882b8a273d25f18c126
Maria Comas <unamaria@gmail.com>
2019-01-25 17:23:31 +0100

    Add storybook.

---
+++ stories/index.js
@@ -0,0 +1,13 @@
+/* global document */
+import { storiesOf } from '@storybook/html';
+
+
+// @TODO: remove demo content.
+storiesOf('Demo', module)
+  .add('heading', () => '<h1>Hello World</h1>')
+  .add('button', () => {
+    const button = document.createElement('button');
+    button.innerText = 'Hello Button';
+    button.addEventListener('click', e => console.log(e));
+    return button;
+});
