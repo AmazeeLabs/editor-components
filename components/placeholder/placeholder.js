db73b0b06104e8aeab02b1f004d25305c86f60aa
Maria Comas <unamaria@gmail.com>
2019-01-29 17:51:31 +0100

    NOBEL-100 Handle click events.

--- components/placeholder/placeholder.js
+++ components/placeholder/placeholder.js
@@ -4,12 +4,15 @@ class Placeholder extends LitElement {
 	static get properties() {
 		return {
 			addSectionActive: { type: Boolean },
+			showSections: { type: Boolean },
 			sections: { type: Array },
+			addSectionCallback: { type: Function }
 		};
 	}
 	constructor() {
 		super();
-		this.addSectionActive = false;
+		this.addSectionActive = true;
+		this.showSections = true;
 		this.sections = [
 			{ id: 'text', label: 'Text', icon: 'text' },
 			{ id: 'formatted-text', label: 'Formatted', icon: 'formatted-text' },
@@ -52,6 +55,10 @@ class Placeholder extends LitElement {
           position: relative;
           top: 0.7em;
         }
+        .ck-placeholder__insert-button:hover,
+        .ck-placeholder__insert-button:focus {
+          color: var(--color-black, #222330);
+        }
         .ck-placeholder__add-wrapper {
           align-items: center;
           border: 1px dotted var(--color-black, #222330);
@@ -61,48 +68,83 @@ class Placeholder extends LitElement {
           padding: 1.9em 1.8em 2em;
         }
         .ck-placeholder__add-button {
-          color: var(--color-blue, #004ADC);
+          color: var(--color-black, #222330);
           font-weight: bold;
           margin-right: 40px;
           text-transform: uppercase;
         }
-        .ck-placeholder__section {
+        .ck-placeholder__add-button:hover,
+        .ck-placeholder__add-button:focus {
+          color: var(--color-blue, #004ADC);
+        }
+        .ck-placeholder__section-item {
           color: var(--color-black, #222330);
           display: inline-block;
           font-size: 12px;
           margin-right: 20px;
           text-transform: uppercase;
         }
+        .ck-placeholder__section-button:hover,
+        .ck-placeholder__section-button:focus {
+          color: var(--color-blue, #004ADC);
+        }
+        .ck-placeholder__close-button {
+          margin-left: auto;
+        }
       </style>
       ${ this.addSectionActive ?
 		html`
-      <div class="ck-placeholder__add-wrapper">
-        <button @click="${ this.clickAddHandler }" type="button" class="normalize-button ck-placeholder__add-button">Add</button>
-        <ul class="normalize-list ck-placeholder__sections-list">
-          ${ this.sections.map( section => html`
-            <li class="ck-placeholder__section">
-              <button type="button" class="normalize-button">${ section.label }</button>
-            </li>   
-          ` ) }    
-        </ul>
-      </div>
-		` :
+          <div class="ck-placeholder__add-wrapper">
+            <button @click="${ this.clickAddHandler }" type="button" class="normalize-button ck-placeholder__add-button">Add</button>
+            ${ this.showSections ?
+		html`<ul class="normalize-list ck-placeholder__sections-list">
+              ${ this.sections.map( section => html`
+                <li class="ck-placeholder__section-item">
+                  <button 
+                    @click="${ event => this.clickSectionHandler( event, section.id ) }" 
+                    type="button" 
+                    class="normalize-button ck-placeholder__section-button"
+                  >
+                    ${ section.label }
+                  </button>
+                </li>   
+              ` ) }    
+            </ul>
+            <button @click="${ this.clickCloseHandler }" type="button" class="normalize-button ck-placeholder__close-button">Close</button>
+            ` : '' }
+          </div>
+        ` :
 		html`
-        <div class="ck-placeholder__insert-wrapper">
-          <button 
-            @click="${ this.clickHandler }" 
-            type="button" 
-            class="normalize-button ck-placeholder__insert-button"
-          >
-            Insert Section
-          </button>
-        </div>
-     ` }
+          <div class="ck-placeholder__insert-wrapper">
+            <button 
+              @click="${ this.clickInsertHandler }" 
+              type="button" 
+              class="normalize-button ck-placeholder__insert-button"
+            >
+              Insert Section
+            </button>
+          </div>
+        ` }
     `;
 	}
 
-	clickHandler() {
-		this.addSectionActive = !this.addSectionActive;
+	clickAddHandler() {
+		this.showSections = !this.showSections;
+	}
+
+	clickInsertHandler() {
+		this.addSectionActive = true;
+	}
+
+	clickCloseHandler() {
+		this.addSectionActive = false;
+	}
+
+	clickSectionHandler( event, sectionId ) {
+		// @TODO: Implement real callback.
+		// eslint-disable-next-line no-undef
+		console.log( sectionId );
+		// this.addSectionCallback(sectionId);
 	}
 }
 
