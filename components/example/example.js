951e10c6e6923cdbdbcb23838ae6cd13e3e4beeb
Maria Comas <unamaria@gmail.com>
2019-01-31 11:23:29 +0100

    Raw loader test (not working).

--- components/example/example.js
+++ components/example/example.js
@@ -1,8 +1,13 @@
 import { LitElement, html } from "lit-element";
+import styles from "./example.css";
 
 class ExampleElement extends LitElement {
   render() {
+    console.log(styles);
     return html`
+      <style>
+        ${styles}
+      </style>
       <p>This is in the shadow Dom!</p>
     `;
   }
