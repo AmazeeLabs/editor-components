d6f8ab3229e3cd8c96642f6aeceb55a08d2f4335
Mattia Simonato <mattiasimonato.w@gmail.com>
2019-02-07 18:34:17 +0100

    Fix icons and add custom property on gallery positioning

--- components/gallery/icon/assets/rightArrow.js
+++ components/gallery/icon/assets/rightArrow.js
@@ -1,7 +1,7 @@
 import { svg } from "lit-element/lit-element";
 
 const iconArrowRight = svg`
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0,0H24V24H0Z" fill="none"/><path d="M12,4,10.59,5.41,16.17,11H4v2H16.17l-5.58,5.59L12,20l8-8Z"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0,0H24V24H0Z" fill="none"/><path d="M12,4,10.59,5.41,16.17,11H4v2H16.17l-5.58,5.59L12,20l8-8Z"/></svg>
 `;
 
 export default iconArrowRight;
--- components/gallery/icon/assets/trash.js
+++ components/gallery/icon/assets/trash.js
@@ -1,7 +1,7 @@
 import { svg } from "lit-element/lit-element";
 
 const iconTrash = svg`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><path d="M6,19a2.006,2.006,0,0,0,2,2h8a2.006,2.006,0,0,0,2-2V7H6ZM8,9h8V19H8Zm7.5-5-1-1h-5l-1,1H5V6H19V4Z"/><path d="M0,0H24V24H0Z" fill="none"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6,19a2.006,2.006,0,0,0,2,2h8a2.006,2.006,0,0,0,2-2V7H6ZM8,9h8V19H8Zm7.5-5-1-1h-5l-1,1H5V6H19V4Z"/><path d="M0,0H24V24H0Z" fill="none"/></svg>
 `;
 
 export default iconTrash;
