15b062b0ec171b4d2779a24759c8c6071bc560c3
Mattia Simonato <mattiasimonato.w@gmail.com>
2019-02-27 10:53:50 +0100

    fix conflicts issues

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -97,7 +97,6 @@ class Tabs extends LitElement {
       <style>
         ${styles}
       </style>
-
       <div class="ck-tabs">
         <div class="ck-tabs__header">
           <ul class="ck-tabs__header-tab-list">
@@ -108,20 +107,6 @@ class Tabs extends LitElement {
             ></li>
           </ul>
         </div>
-        <ck-modal
-          @eventCloseModal="${() => {
-            this.closeModal();
-          }}"
-          @eventSaveModal="${e => {
-            this.saveModal(e.detail);
-          }}"
-          @deleteTab="${() => this.deleteItem()}"
-          currentTitle="${this.items[this.currentTab].title}"
-          currentDefault="${this.items[this.currentTab].default}"
-          currentIndex="${this.currentTab}"
-          data-visible="${this.modalIsOpen ? "true" : "false"}"
-        >
-        </ck-modal>
         <div class="ck-tabs__content">
           <div
             class="ck-tabs__rail"
@@ -289,7 +274,6 @@ class Modal extends LitElement {
       <style>
         ${modalStyles}
       </style>
-
       <div class="modal ${this.isVisible ? "visible" : ""}">
         <div class="modal__item">
           <h3 class="modal__title">Edit tab</h3>
@@ -340,135 +324,5 @@ class Modal extends LitElement {
   }
 }
 
-class Modal extends LitElement {
-  static get properties() {
-    return {
-      isVisible: Boolean,
-      inputText: String,
-      items: Array,
-      isDefault: Boolean,
-      currentDefault: String,
-      currentIndex: String,
-      currentTitle: String
-    };
-  }
-
-  constructor() {
-    super();
-    this.isVisible = false;
-    this.inputText = "";
-    this.isDefault = false;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    const self = this;
-    const observer = new MutationObserver(mutationsList => {
-      mutationsList.forEach(mutation => {
-        if (mutation.type === "attributes") {
-          if (self.dataset.visible === "true") {
-            self.isVisible = true;
-            self.isDefault = self.currentDefault === "true";
-            self.inputText = self.currentTitle;
-          }
-        }
-      });
-    });
-
-    observer.observe(self, {
-      attributes: true,
-      childList: true,
-      subtree: false
-    });
-
-    self.isDefault = self.currentDefault === "true";
-    self.isVisible = self.dataset.visible === "true";
-  }
-
-  closeModal() {
-    this.dispatchEvent(new Event("eventCloseModal"));
-    this.isVisible = false;
-  }
-
-  handleInput(e) {
-    this.inputText = e.target.value;
-  }
-
-  saveModal() {
-    const response = {
-      index: this.currentIndex,
-      title: this.inputText,
-      default: this.isDefault
-    };
-    this.dispatchEvent(new CustomEvent("eventSaveModal", { detail: response }));
-    this.closeModal();
-  }
-
-  handleSwitch(e) {
-    this.isDefault = e.target.checked;
-  }
-
-  deleteTab() {
-    this.dispatchEvent(new Event("deleteTab"));
-    this.closeModal();
-  }
-
-  render() {
-    return html`
-      <style>
-        ${modalStyles}
-      </style>
-
-      <div class="modal ${this.isVisible ? "visible" : ""}">
-        <div class="modal__item">
-          <h3 class="modal__title">Edit tab</h3>
-          <div class="modal__content">
-            <label class="modal__label" for="${this.icurrentIndex}">
-              Variation name
-            </label>
-            <input
-              class="modal__input"
-              id="${this.currentIndex}"
-              @input=${this.handleInput}
-              .value=${this.currentTitle}
-            />
-            <div class="modal__toggle">
-              <label class="switch">
-                <input
-                  type="checkbox"
-                  .checked="${this.isDefault}"
-                  @input="${e => this.handleSwitch(e)}"
-                />
-                <span class="slider"></span>
-              </label>
-              <span class="modal__toggle-label">Set as default</span>
-            </div>
-          </div>
-          <div class="modal__actions">
-            <div class="modal__action-wrap-delete">
-              <span
-                class="modal__action modal__action--delete"
-                @click="${() => this.deleteTab()}"
-              >
-                Delete
-              </span>
-            </div>
-            <span class="modal__action" @click="${() => this.closeModal()}">
-              Cancel
-            </span>
-            <span
-              class="modal__action modal__action--primary"
-              @click="${() => this.saveModal()}"
-            >
-              save
-            </span>
-          </div>
-        </div>
-      </div>
-    `;
-  }
-}
-
-customElements.define("ck-modal", Modal);
 customElements.define("ck-tabs", Tabs);
 customElements.define("ck-tabs-modal", Modal);
