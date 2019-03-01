1052bc752599f2484e29dace8182e68db1c02ff8
Philipp Melab <philipp.melab@gmail.com>
2019-03-01 12:49:37 +0100

    Setting a default height for media items.

--- components/media/media.stories.js
+++ components/media/media.stories.js
@@ -3,8 +3,7 @@ import "./media";
 
 storiesOf("Media", module).add("Default", () => {
   const media = document.createElement("ck-media");
-  media.style.width = "500px";
-  media.style.height = "300px";
+  media.style.maxWidth = "500px";
   media.style.display = "block";
   media.setAttribute("data-media-uuid", ``);
   media.setAttribute("data-media-display", `default`);
