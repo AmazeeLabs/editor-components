50f37039e5a97b63217db222060c73a2fc975853
Mattia Simonato <mattiasimonato.w@gmail.com>
2019-03-13 11:02:59 +0100

    Character limitation component.
    * Create style and markup structure
    
    * Implement tooltip and helper functionality and validation
    
    * Fixed live update.
    
    Updated max-constraint in any case on typing.
    Validate immediately if flag is set.
    
    * update style to use variables for spacing and sizing
    
    * handle messages on different type of validations

--- components/textfield/textfield.js
+++ components/textfield/textfield.js
@@ -1,65 +1,162 @@
 import { LitElement, html, css } from "lit-element";
+import styles from "./textfield.css";
 
 export default class TextField extends LitElement {
   static get properties() {
     return {
       pattern: { attribute: "ck-pattern", type: String },
       hasPatternError: { type: Boolean },
-      patternErrorMessage: { attribute: "ck-message-error", type: String },
+      errorMessage: { attribute: "ck-error-message", type: String },
       minLength: { attribute: "ck-min", type: Number },
       maxLength: { attribute: "ck-max", type: Number },
       hasLengthError: { type: Boolean },
+      hasHelper: { type: Boolean },
       helper: { attribute: "ck-message-helper", type: String }
     };
   }
 
   connectedCallback() {
     super.connectedCallback();
+
     new MutationObserver(this.validate).observe(this, {
       childList: true,
       subtree: true
     });
-    this.addEventListener("input", this.validate);
+
+    this.addEventListener("focus", this.setHelper);
+
+    this.addEventListener("blur", () => {
+      this.validate();
+      this.hasHelper = false;
+    });
+
+    if (this.maxLength && !this.minLength) {
+      this.addEventListener("input", this.handleMax);
+    }
+
+    // textfield errors immediately highlighted
+    if (TextField.initializeWithErrors) this.validate();
+  }
+
+  handleMax() {
+    this.helper = `${this.maxLength - this.innerText.length} characters remaining`;
+    this.setHelper();
+    this.maxValidation();
   }
 
   validate() {
-    console.log(this.innerText);
+    if (!this.innerText) return;
+    // MAX
+    if (this.hasAttribute("ck-max")) this.maxValidation();
+    // MIN
+    if (this.hasAttribute("ck-min")) this.minValidation();
+    // Range
+    if (this.hasAttribute("ck-max") && this.hasAttribute("ck-min")) this.rangeValidation();
+    // Pattern
+    if (this.hasAttribute("ck-pattern")) this.patternValidation();
+  }
+
+  maxValidation() {
+    if (this.innerText.length > this.maxLength && !this.minLength) {
+      if (!this.errorMessage) {
+        this.errorMessage = `Please enter no more than 
+          ${this.maxLength} letters.`;
+      }
+      this.hasLengthError = true;
+    } else {
+      this.hasLengthError = false;
+    }
+    this.setHelper();
+  }
+
+  minValidation() {
+    if (this.innerText.length < this.minLength) {
+      if (!this.errorMessage) {
+        this.errorMessage = `Please enter at least 
+          ${this.minLength} letters.`;
+      }
+      this.hasLengthError = true;
+    } else {
+      this.hasLengthError = false;
+    }
+  }
+
+  rangeValidation() {
+    if (
+      this.innerText.length > this.minLength &&
+      this.innerText.length < this.maxLength
+    ) {
+      this.hasLengthError = false;
+    } else {
+      if (!this.errorMessage) {
+        this.errorMessage = `Please enter letters between 
+        ${this.minLength} and ${this.maxLength}.`;
+      }
+      this.hasLengthError = true;
+    }
+  }
+
+  patternValidation() {
+    console.log(this.errorMessage)
+    const pattern = new RegExp(this.pattern);
+    if (pattern.test(this.innerText)) {
+      this.hasPatternError = false;
+    } else {
+      this.hasPatternError = true;
+    }
+  }
+
+  setHelper() {
+    if (this.helper) {
+      if (!this.hasPatternError && !this.hasLengthError) {
+        this.hasHelper = true;
+      } else {
+        this.hasHelper = false;
+      }
+    }
   }
 
   render() {
-    // language=HTML
     return html`
-      ${this.helper
-        ? html`
-            <div class="helper">${this.helper}</div>
-          `
-        : null}
-      ${this.hasLengthError
-        ? html`
-            <div class="error">${"Length error"}</div>
-          `
-        : null}
-      ${this.hasPatternError
-        ? html`
-            <div class="error">${"Pattern error"}</div>
-          `
-        : null}
+      <style>
+        ${styles}
+      </style>
+
       <div
-        class="${this.hasPatternError || this.hasLengthError
-          ? "is-valid"
-          : "is-invalid"}"
+        class="ck-textfield ${this.hasPatternError || this.hasLengthError
+          ? "error"
+          : ""}"
       >
-        <slot></slot>
+        ${this.hasHelper
+          ? html`
+              <div class="ck-tooltip ck-tooltip--helper">${this.helper}</div>
+            `
+          : null}
+        ${this.hasLengthError
+          ? html`
+              <div class="ck-tooltip ck-tooltip--error">
+                ${this.errorMessage ? this.errorMessage : "Length error"}
+              </div>
+            `
+          : null}
+        ${this.hasPatternError
+          ? html`
+              <div class="ck-tooltip ck-tooltip--error">
+                ${this.errorMessage ? this.errorMessage : "Pattern error"}
+              </div>
+            `
+          : null}
+        <div
+          class="${this.hasPatternError || this.hasLengthError
+            ? "is-valid"
+            : "is-invalid"}"
+        >
+          <slot></slot>
+        </div>
       </div>
     `;
   }
 }
-
-TextField.styles = css`
-  :host {
-    display: block;
-  }
-`;
 
 // Static flag if textfield errors should be highlighted immediately.
 // If set to false, errors are highlighted after focus is lost for the first time.
--- components/textfield/textfield.stories.js
+++ components/textfield/textfield.stories.js
@@ -7,25 +7,26 @@ storiesOf("Textfield", module)
   .addDecorator(Editor.decorator)
   .add(
     "Simple",
-    () => `<ck-textfield contenteditable="true">This is editable</ck-textfield>`
+    () =>
+      `<ck-textfield style="margin-top: 40px" ck-message-helper="here to help you" contenteditable="true">This is editable</ck-textfield>`
   )
   .add(
     "Min",
     () =>
-      `<ck-textfield ck-min="3" contenteditable="true">This is editable</ck-textfield>`
+      `<ck-textfield style="margin-top: 40px" ck-min="3" ck-message-helper="here to help you" contenteditable="true">This is editable</ck-textfield>`
   )
   .add(
     "Max",
     () =>
-      `<ck-textfield ck-max="5" contenteditable="true">This is editable</ck-textfield>`
+      `<ck-textfield style="margin-top: 40px" ck-max="5" contenteditable="true">This is editable</ck-textfield>`
   )
   .add(
     "Range",
     () =>
-      `<ck-textfield ck-min="3" ck-max="5" contenteditable="true">This is editable</ck-textfield>`
+      `<ck-textfield style="margin-top: 40px" ck-min="3" ck-max="9" ck-message-helper="here to help you" contenteditable="true">This is editable</ck-textfield>`
   )
   .add(
     "Pattern",
     () =>
-      `<ck-textfield ck-pattern="[abc]" ck-error-message="Please use only a, b or c." contenteditable="true">This is editable</ck-textfield>`
+      `<ck-textfield style="margin-top: 40px" ck-pattern="[abc]" ck-message-helper="here to help you" ck-error-message="Please use only a, b or c." contenteditable="true">This is editable</ck-textfield>`
   );
