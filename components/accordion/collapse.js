f4eaa9616cbcde94791799142f577336a319647b
Mattia Simonato <mattiasimonato.w@gmail.com>
2019-05-21 11:17:40 +0200

    Create simple collapse componet

---
+++ components/accordion/collapse.js
@@ -0,0 +1,52 @@
+import { html, svg } from "lit-element";
+import styles from "!raw-loader!./collapse.css";
+import EditorElement from "../base/editor-element/editor-element";
+
+import arrowIcon from "!raw-loader!./icons/leftArrow.svg";
+import trashIcon from "!raw-loader!./icons/trash.svg";
+
+export default class Collapse extends EditorElement {
+  static get properties() {
+    return {
+      collapseIsOpen: {
+        type: Boolean,
+        attribute: "ck-collapse-open",
+        reflect: true
+      }
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    if (this.collapseIsOpen == null) {
+      this.collapseIsOpen = false;
+    }
+  }
+
+  toggleCollapse() {
+    this.collapseIsOpen = !this.collapseIsOpen;
+  }
+
+  render() {
+    return html`
+      <style>
+        ${styles}
+      </style>
+
+      <div class="ck-collapse ${this.collapseIsOpen ? "open" : ""}">
+        <div class="ck-collapse__header">
+          <div class="ck-collapse__title">
+            <slot name="title"></slot>
+          </div>
+
+          <div class="ck-collapse__action" @click=${this.toggleCollapse}></div>
+
+        </div>
+        <div class="ck-collapse__body">
+          <slot></slot>
+        </div>
+      </div>
+    `;
+  }
+}
