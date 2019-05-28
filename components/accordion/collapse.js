7c8219bb5414f83187b7215206574685665ed15c
Mattia Simonato <mattiasimonato.w@gmail.com>
2019-05-28 14:24:30 +0200

    Create collapse web component

--- components/accordion/collapse.js
+++ components/accordion/collapse.js
@@ -1,65 +1,25 @@
 import { html, svg } from "lit-element";
 import styles from "!raw-loader!./collapse.css";
-import EditorElement from "../base/editor-element/editor-element";
+import Section from "../section/section";
 
-import arrowIcon from "!raw-loader!./icons/leftArrow.svg";
-import trashIcon from "!raw-loader!./icons/trash.svg";
-
-export default class Collapse extends EditorElement {
+export default class Collapse extends Section {
   static get properties() {
     return {
       collapseIsOpen: {
-        type: Boolean,
-        attribute: "ck-collapse-open",
-        reflect: true
-      },
-      collapseIndex: {
-        type: Number,
-        attribute: "ck-collapse-index",
-        reflect: true
-      },
-      arrowUpIsActive: {
-        type: Boolean,
-        attribute: "ck-collapse-arrow-up",
-        reflect: true
-      },
-      arrowDownIsActive: {
-        type: Boolean,
-        attribute: "ck-collapse-arrow-down",
-        reflect: true
+        type: Boolean
       }
     };
   }
 
   connectedCallback() {
     super.connectedCallback();
-
     if (this.collapseIsOpen == null) {
       this.collapseIsOpen = false;
-    }
-    if (this.arrowUpIsActive == null) {
-      this.arrowUpIsActive = false;
-    }
-    if (this.arrowDownIsActive == null) {
-      this.arrowDownIsActive = false;
     }
   }
 
   toggleCollapse() {
     this.collapseIsOpen = !this.collapseIsOpen;
-  }
-
-  moveItem(position) {
-    if (position === "up" && this.arrowUpIsActive) {
-      this.modifyDocument(editor =>
-        editor.move(this, "before", this.collapseIndex, this.collapseIndex - 1)
-      );
-    }
-    if (position === "down" && this.arrowDownIsActive) {
-      this.modifyDocument(editor =>
-        editor.move(this, "before", this.collapseIndex, this.collapseIndex + 1)
-      );
-    }
   }
 
   render() {
@@ -75,27 +35,8 @@ export default class Collapse extends EditorElement {
           </div>
 
           <div class="ck-collapse__action" @click=${this.toggleCollapse}></div>
-
-          <div class="ck-collapse__icons">
-            <div
-              @click=${() => this.moveItem("up")}
-              class="ck-collapse__icon ck-collapse__icon--arrow-up
-                ${this.arrowUpIsActive ? "" : "disabled"}"
-            >
-              ${svg([arrowIcon])}
-            </div>
-            <div
-              @click=${() => this.moveItem("down")}
-              class="ck-collapse__icon ck-collapse__icon--arrow-down
-                ${this.arrowDownIsActive ? "" : "disabled"}"
-            >
-              ${svg([arrowIcon])}
-            </div>
-            <div class="ck-collapse__icon ck-collapse__icon--trash">
-              ${svg([trashIcon])}
-            </div>
-          </div>
         </div>
+
         <div class="ck-collapse__body">
           <slot></slot>
         </div>
--- components/accordion/collapse.stories.js
+++ components/accordion/collapse.stories.js
@@ -9,19 +9,13 @@ storiesOf("Accordion/", module)
   .add(
     "Collapse",
     () => `
-    <ck-collapse ck-collapse-open ck-collapse-arrow-down ck-collapse-index="0">
+    <ck-collapse>
       <p slot="title">Collapse Title</p>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </div>
     </ck-collapse>
-    <ck-collapse  ck-collapse-arrow-up ck-collapse-arrow-down  ck-collapse-index="1">
-      <p slot="title">Collapse Title</p>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </div>
-    </ck-collapse>
-    <ck-collapse ck-collapse-arrow-up  ck-collapse-index="2">
+    <ck-collapse>
       <p slot="title">Collapse Title</p>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
